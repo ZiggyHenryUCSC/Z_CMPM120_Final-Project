ae9275a779018ad7f86a817c1e5b8cd2745d515e
ZiggyHenryUCSC <zchenry@ucsc.edu>
2026-06-08 15:26:23 -0700

    load title screen on start
    previously changed for testing

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -91,9 +91,7 @@ class Load extends Phaser.Scene {
         //};
 
          // ...and pass to the next Scene
-        this.scene.start("platformerScene", {vfx: this.vfx});
-        
-        //this.scene.start("titleScene", {vfx: this.vfx});
+        this.scene.start("titleScene", {vfx: this.vfx});
     }
 
     // Never get here since a new scene is started in create()
